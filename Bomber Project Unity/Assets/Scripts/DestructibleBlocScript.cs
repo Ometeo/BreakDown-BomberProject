64bac8625128126dc9b0b401584a533721676060
Ometeo <jonathan.bihet@gmail.com>
2013-10-17 13:08:18 +0200

    [Add] Base Prefab
    Ajout des prefabs suivant :
    - BasicDestructibleBloc
    - GroundBloc
    - IndestructibleBloc
    - ClassyBorder
    - ClassyGround
    
    Ajout des scripts suivants :
    - DestructibleBlocScript
    - GroundScript
    
    une animation à renommer : NewAnimation
    
    les matériaux suivants :
    -BasicDestructibleBloc
    -GroundMaterial
    -IndestructibleBlocMat
    
    ...

---
+++ Bomber Project Unity/Assets/Scripts/DestructibleBlocScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// to test for animation http://itween.pixelplacement.com/index.php
+
+public class DestructibleBlocScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private int _nbHP;
+
+    public int NbHP
+    {
+        get
+        {
+            return _nbHP;
+        }
+        set
+        {
+            _nbHP = value;
+        }
+    }
+
+    private Animation _destroyAnimation;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _destroyAnimation = this.gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NbHP <= 0)
+        {
+            _destroyAnimation.Play();
+            
+            DestroyBloc();
+        }
+    }
+
+    void DestroyBloc()
+    {
+        if(this.gameObject.transform.localScale == Vector3.zero)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Bomber Project Unity/Assets/Scripts/GroundScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundScript : MonoBehaviour {
+
+    [SerializeField]
+    private bool isSpawn;
+    public bool IsSpawn
+    {
+        get
+        {
+            return isSpawn;
+        }
+        set
+        {
+            isSpawn = value;
+        }
+    }
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
