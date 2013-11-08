785bb4c632b3e66ac0be3575b01f01019f0d244a
Rominet <thierry.destribats@gmail.com>
2013-11-08 21:44:36 +0100

    [Add] CameraView (Jonathan's Work)

---
+++ Bomber Project Unity/Assets/Scripts/CameraViewScripts/CameraBorderScript.cs
@@ -0,0 +1,58 @@
+﻿/* --------------------------Header-------------------------------------
+ * File : CameraBorderScript.cs
+ * Description : Script that handles the borders of an arena to stop camera moving.
+ * Version : 1.0.0
+ * Created Date : 05/11/2013 11:46:46
+ * Created by : Jonathan Bihet
+ * Modification Date : 05/11/2013 18:08:33
+ * Modified by : Jonathan Bihet
+ * ------------------------------------------------------------------------ */
+
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraBorderScript : MonoBehaviour
+{
+    private enum _BLabel { LEFT = 0, RIGHT, FRONT, BACK };
+
+    [SerializeField]
+    private GameObject[] _borders;
+    public GameObject[] Borders
+    {
+        get { return _borders; }
+        set { _borders = value; }
+    }
+
+    [SerializeField]
+    private Vector2 _arenaSize;
+    public Vector2 ArenaSize
+    {
+        get { return _arenaSize; }
+        set { _arenaSize = value; }
+    }
+
+    void Start()
+    {
+        int positionX = (int)(ArenaSize[0] / 2);
+        Borders[(int)(_BLabel.LEFT)].transform.position = new Vector3(-positionX, 0.5f, 0.0f);
+        BoxCollider col = (BoxCollider)Borders[(int)(_BLabel.LEFT)].collider;
+        col.size = new Vector3(1, 1, ArenaSize[1]);
+
+
+        Borders[(int)(_BLabel.RIGHT)].transform.position = new Vector3(positionX, 0.5f, 0.0f);
+        col = (BoxCollider)Borders[(int)(_BLabel.RIGHT)].collider;
+        col.size = new Vector3(1, 1, ArenaSize[1]);
+
+        int positionY = (int)(ArenaSize[1] / 2);
+        Borders[(int)(_BLabel.FRONT)].transform.position = new Vector3(0.0f, 0.5f, positionY);
+        col = (BoxCollider)Borders[(int)(_BLabel.FRONT)].collider;
+        col.size = new Vector3(ArenaSize[0], 1, 1);
+
+
+        Borders[(int)(_BLabel.BACK)].transform.position = new Vector3(0.0f, 0.5f, -positionY);
+        col = (BoxCollider)Borders[(int)(_BLabel.BACK)].collider;
+        col.size = new Vector3(ArenaSize[0], 1, 1);
+
+    }
+}
---
+++ Bomber Project Unity/Assets/Scripts/CameraViewScripts/CameraViewScript.cs
@@ -0,0 +1,152 @@
+﻿/* --------------------------Header-------------------------------------
+ * File : CameraViewScript.cs
+ * Description : 
+ * Version : 1.0.0
+ * Created Date : 05/11/2013 18:08:38
+ * Created by : Jonathan Bihet
+ * Modification Date : 06/11/2013 10:51:27
+ * Modified by : Jonathan Bihet
+ * ------------------------------------------------------------------------ */
+
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraViewScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _cameraSpeed;
+    public float CameraSpeed
+    {
+        get { return _cameraSpeed; }
+        set { _cameraSpeed = value; }
+    }
+
+    [SerializeField]
+    private Transform[] _players;
+    public Transform[] Players
+    {
+        get { return _players; }
+        set { _players = value; }
+    }
+
+    [SerializeField]
+    private Transform _currentPlayer;
+
+    [SerializeField]
+    private KeyCode _centerOnPlayerKey;
+    public KeyCode CenterOnPlayerKey
+    {
+        get { return _centerOnPlayerKey; }
+        set { _centerOnPlayerKey = value; }
+    }
+
+    private bool _canMoveLeft = false;
+    private bool _canMoveRight = false;
+    private bool _canMoveFront = false;
+    private bool _canMoveBack = false;
+
+    private float _mouseBorderDetect = 30.0f;
+    private Vector3 _cameraDirection;
+
+    void Start()
+    {
+        if (Network.connections.Length == 0)
+            EnableMoving(false);
+        else
+            EnableMoving(true);
+    }
+
+    void Update()
+    {
+        _cameraDirection = Vector3.zero;
+
+        if (Input.mousePosition.x >= Screen.width - _mouseBorderDetect)
+        {
+            if (_canMoveRight)
+                _cameraDirection.x = 1.0f;
+        }
+        if (Input.mousePosition.x <= _mouseBorderDetect)
+        {
+            if (_canMoveLeft)
+                _cameraDirection.x = -1.0f;
+        }
+
+        if (Input.mousePosition.y >= Screen.height - _mouseBorderDetect)
+        {
+            if (_canMoveFront)
+                _cameraDirection.z = 1.0f;
+        }
+        if (Input.mousePosition.y <= _mouseBorderDetect)
+        {
+            if (_canMoveBack)
+                _cameraDirection.z = -1.0f;
+        }
+
+        transform.Translate(_cameraDirection.normalized * CameraSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name.Equals("LeftBorder"))
+        {
+            _canMoveLeft = false;
+        }
+        if (other.transform.name.Equals("RightBorder"))
+        {
+            _canMoveRight = false;
+        }
+        if (other.transform.name.Equals("FrontBorder"))
+        {
+            _canMoveFront = false;
+        }
+        if (other.transform.name.Equals("BackBorder"))
+        {
+            _canMoveBack = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.name.Equals("LeftBorder"))
+        {
+            _canMoveLeft = true;
+        }
+        if (other.transform.name.Equals("RightBorder"))
+        {
+            _canMoveRight = true;
+        }
+        if (other.transform.name.Equals("FrontBorder"))
+        {
+            _canMoveFront = true;
+        }
+        if (other.transform.name.Equals("BackBorder"))
+        {
+            _canMoveBack = true;
+        }
+    }
+
+    void EnableMoving(bool value)
+    {
+        _canMoveLeft = value;
+        _canMoveRight = value;
+        _canMoveFront = value;
+        _canMoveBack = value;
+    }
+
+    void OnServerInitialized()
+    {
+        EnableMoving(false);
+    }
+
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        EnableMoving(true);
+    }
+
+    void OnConnectedToServer()
+    {
+        EnableMoving(true);
+    }
+}
