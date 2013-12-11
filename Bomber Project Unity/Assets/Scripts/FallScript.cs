6ec7d7594f2e27150a2b96823c5beda5760e24b2
Ometeo / Jonathan Bihet <jonathan.bihet@gmail.com>
2013-12-11 10:06:58 +0100

    Interface edgy + tiny + debut chute

---
+++ Bomber Project Unity/Assets/Scripts/FallScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.LogError("test1");
+        if (Network.isServer)
+        {
+            Debug.LogError("test2");
+            if (col.CompareTag("Player"))
+            {
+                Debug.LogError("plop");
+                col.transform.parent.GetComponentInChildren<ChampionsStatsScript>().LifePoint = 0;
+            }
+        }
+    }
+
+}
